8de46b88a82c5c3a45ea84baa4049f8177da89b1
Michael Franzl <office@michaelfranzl.com>
2013-12-09 17:04:43 +0100

    changed tabs to spaces, adapted plugin paths for SAAS operation

--- wcjsonapi_sr.js
+++ wcjsonapi_sr.js
@@ -1,73 +1,76 @@
 wcjsonapi_sr.createSlugFromName = function (name) {
-	var slug = name;
-	slug = slug.toLowerCase();
-	slug = slug.replace(/[^a-z]/g, "-");
-	return slug;
+    var slug = name;
+    slug = slug.toLowerCase();
+    slug = slug.replace(/[^a-z]/g, "-");
+    return slug;
 }
 wcjsonapi_sr.send_to_wp = function (sender,item) {
-	if ( ! wcjsonapi_sr.website_categories ) {
-		wcjsonapi_sr.maybeShowErrors({errors: [ {text: "Please wait until cats are loaded."} ]});
-		return;
-	}
-	console.log(item);
-	var product = {
-		name: item.name,
-		sku: item.sku,
-		price: Round(item.price_cents / 100,2),
-		description: item.description,
-		product_type: 'simple',
-		type: 'product'
-	}
-	
+    if ( ! wcjsonapi_sr.website_categories ) {
+        wcjsonapi_sr.maybeShowErrors({errors: [ {text: "Please wait until cats are loaded."} ]});
+        return;
+    }
+    console.log(item);
+    var product = {
+        name: item.name,
+        sku: item.sku,
+        price: Round(item.price_cents / 100,2),
+        description: item.description,
+        product_type: 'simple',
+        type: 'product'
+    }
+    
 
-	var find_req = wcjsonapi_sr.prepareRequest('get_products');
-	find_req.arguments.skus = [item.sku]
-	wcjsonapi_sr.getRequest(find_req,function (data) {
-		if ( data.payload.length > 0 ) {
-			var website_product = data.payload[0];
-			console.log("Website Product Is: ", website_product);
-			for ( k in product ) {
-				if ( k == "description" || k == "categories" ) {
-					// We don't override the desc.
-					continue;
-				}
-				website_product[k] = product[k];
-			}
-			product = website_product;
-		} else {
-			var salor_category = null;
-			for ( i in Categories ) {
-				var cat = Categories[i];
-				if ( cat.id == item.category_id ) {
-					salor_category = cat;
-					break;
-				}
-			}
-			if ( salor_category ) {
-				var website_category = null;
-				for (i in wcjsonapi_sr.website_categories ) {
-					var cat = wcjsonapi_sr.website_categories[i];
-					if ( cat.name == salor_category.name ) {
-						website_category = cat;
-						break;
-					}
-				}
-				if ( ! website_category ) {
-					website_category = {taxonomy: 'product_cat',name: salor_category.name, slug: wcjsonapi_sr.createSlugFromName(salor_category.name) }
-				}
-				product.categories = [website_category];
-			}
-		}
-		console.log("Product is: ", product);
-		var req = wcjsonapi_sr.prepareRequest('set_products');
-		req.payload = [product];
-		req.wcjsonapi_sr_sender = sender.attr('id');
-		wcjsonapi_sr.getRequest(req,function (data) {
-			console.log("set_products", data);
-			wcjsonapi_sr.maybeShowErrors(data);
-			wcjsonapi_sr.maybeShowNotifications(data);
-		});
-	});
+    var find_req = wcjsonapi_sr.prepareRequest('get_products');
+    find_req.arguments.skus = [item.sku];
+    displayMessage("notice","wcjsonapi_sr: Getting Products...");
+    wcjsonapi_sr.getRequest(find_req,function (data) {
+        if ( data.payload.length > 0 ) {
+            var website_product = data.payload[0];
+            console.log("Website Product Is: ", website_product);
+            for ( k in product ) {
+                if ( k == "description" || k == "categories" ) {
+                    // We don't override the desc.
+                    continue;
+                }
+                website_product[k] = product[k];
+            }
+            product = website_product;
+        } else {
+            var salor_category = null;
+            for ( i in Categories ) {
+                var cat = Categories[i];
+                if ( cat.id == item.category_id ) {
+                    salor_category = cat;
+                    break;
+                }
+            }
+            if ( salor_category ) {
+                var website_category = null;
+                for (i in wcjsonapi_sr.website_categories ) {
+                    var cat = wcjsonapi_sr.website_categories[i];
+                    if ( cat.name == salor_category.name ) {
+                        website_category = cat;
+                        break;
+                    }
+                }
+                if ( ! website_category ) {
+                    website_category = {taxonomy: 'product_cat',name: salor_category.name, slug: wcjsonapi_sr.createSlugFromName(salor_category.name) }
+                }
+                product.categories = [website_category];
+            }
+        }
+        console.log("Product is: ", product);
+        var req = wcjsonapi_sr.prepareRequest('set_products');
+        req.payload = [product];
+        req.wcjsonapi_sr_sender = sender.attr('id');
+        displayMessage("notice","wcjsonapi_sr: Setting Products...");
+        wcjsonapi_sr.getRequest(req,function (data) {
+            displayMessage("notice","wcjsonapi_sr: Done!");
+            console.log("set_products", data);
+            wcjsonapi_sr.maybeShowErrors(data);
+            wcjsonapi_sr.maybeShowNotifications(data);
+        });
+    });
 }
 
 
@@ -79,7 +82,7 @@ wcjsonapi_sr.prepareRequest = function (name,existing_req) {
   req.action = "woocommerce_json_api";
   req.proc = name;
   req.arguments = {
-  	token: wcjsonapi_sr.meta.token
+    token: wcjsonapi_sr.meta.token
   };
   req.errors = [];
   req.payload = [];
@@ -89,19 +92,19 @@ wcjsonapi_sr.prepareRequest = function (name,existing_req) {
 }
 
 wcjsonapi_sr.reveal = function (m) {
-	// example: reveal(item) == "[object Object]"
- 	return Object.prototype.toString.call(m);
+    // example: reveal(item) == "[object Object]"
+    return Object.prototype.toString.call(m);
 }
 wcjsonapi_sr.getSettings = function () {
-	return wcjsonapi_sr.meta;
+    return wcjsonapi_sr.meta;
 }
 wcjsonapi_sr.debug = function(txt) {
-	if ( wcjsonapi_sr.meta.debug == 'yes' ) {
-		console.log(txt);
-	}
+    if ( wcjsonapi_sr.meta.debug == 'yes' ) {
+        console.log(txt);
+    }
 }
 wcjsonapi_sr.displayRequestDebug = function (req) {
-	wcjsonapi_sr.debug(req);
+    wcjsonapi_sr.debug(req);
 }
 wcjsonapi_sr.showProgressBar = function () {
 
@@ -113,57 +116,59 @@ wcjsonapi_sr.maybeShowNotifications = function (req) {
 
 }
 wcjsonapi_sr.maybeShowErrors = function (req) {
-	var text = "";
-	if ( req.errors && req.errors.length > 0) {
-		for ( i in req.errors ) {
-			var error = req.errors[i];
-			text += "<li>" + error.text + "</li>"
-		}
-		text = '<ul class="errors">' + text + '</ul>';
-		var dialog = shared.draw.dialog("WooCommerce JSON API Errors",'wcjsonapi_sr_error_dialog',text);
-	}
+    var text = "";
+    if ( req.errors && req.errors.length > 0) {
+        for ( i in req.errors ) {
+            var error = req.errors[i];
+            text += "<li>" + error.text + "</li>"
+        }
+        text = '<ul class="errors">' + text + '</ul>';
+        var dialog = shared.draw.dialog("WooCommerce JSON API Errors",'wcjsonapi_sr_error_dialog',text);
+    }
 
 }
 wcjsonapi_sr.getRequest = function(req, callback, options) {
-	var s = wcjsonapi_sr.getSettings();
-	//$.getJSON(s.url, req, onGetRequestComplete );
-	wcjsonapi_sr.displayRequestDebug(req);
-	wcjsonapi_sr.showProgressBar();
-	$.ajax({
-		type: "POST",
-		url: wcjsonapi_sr.meta.url + "?jsonp=?",
-		data: { json: JSON.stringify(req) },
-		contentType: 'application/json',
-		success: function(data) {
+    var s = wcjsonapi_sr.getSettings();
+    //$.getJSON(s.url, req, onGetRequestComplete );
+    wcjsonapi_sr.displayRequestDebug(req);
+    wcjsonapi_sr.showProgressBar();
+    $.ajax({
+        type: "POST",
+        url: wcjsonapi_sr.meta.url + "?jsonp=?",
+        data: { json: JSON.stringify(req) },
+        contentType: 'application/json',
+        success: function(data) {
 
-			if ( wcjsonapi_sr.meta.debug == 'yes' ) {
-				console.log("getRequest RECEIVED:", data);
-			}
-			wcjsonapi_sr.maybeShowNotifications(data);
-			wcjsonapi_sr.maybeShowErrors(data);
-			wcjsonapi_sr.hideProgressBar();
-			wcjsonapi_sr.statistics = data.statistics;
-			if (callback) {
-				callback(data);
-			}
+            if ( wcjsonapi_sr.meta.debug == 'yes' ) {
+                console.log("getRequest RECEIVED:", data);
+            }
+            wcjsonapi_sr.maybeShowNotifications(data);
+            wcjsonapi_sr.maybeShowErrors(data);
+            wcjsonapi_sr.hideProgressBar();
+            wcjsonapi_sr.statistics = data.statistics;
+            if (callback) {
+                callback(data);
+            }
 
-		},
-		dataType: 'json',
-		error: function (xhr,type) {
-			if ( wcjsonapi_sr.meta.debug == 'yes' )
-				console.log( "ERROR", xhr, type);
-		}
-	});
+        },
+        dataType: 'json',
+        error: function (xhr,type) {
+            if ( wcjsonapi_sr.meta.debug == 'yes' )
+                console.log( "ERROR", xhr, type);
+        }
+    });
 
-	if ( wcjsonapi_sr.meta.debug == 'yes' ) {
-		console.log("getRequest SENT:", req);
-	}
+    if ( wcjsonapi_sr.meta.debug == 'yes' ) {
+        console.log("getRequest SENT:", req);
+    }
 }
 $(function () {
-	var req = wcjsonapi_sr.prepareRequest("get_categories")
-	wcjsonapi_sr.getRequest(req,function (data) {
-		if ( data.status ) {
-			wcjsonapi_sr.website_categories = data.payload;
-		}
-	});
+    var req = wcjsonapi_sr.prepareRequest("get_categories")
+    displayMessage("notice","wcjsonapi_sr: Loading Categories...");
+    wcjsonapi_sr.getRequest(req,function (data) {
+        if ( data.status ) {
+          displayMessage("notice","wcjsonapi_sr: Loading Categories done!");
+          wcjsonapi_sr.website_categories = data.payload;
+        }
+    });
 })
--- wcjsonapi_sr.pl.js
+++ wcjsonapi_sr.pl.js
@@ -1,4 +1,7 @@
-Salor.log_action("Initializing Plugin WCJSON API");
+// Token: 12345
+// Alternative URL: http://clientreview.thebigrede.net/wp-content/plugins/woocommerce-json-api/api.php
+
+Salor.log_action_plugin("wcjsonapi_sr: Initializing Plugin WCJSON API");
 // Use plugins. inside your functions, but don't use plugins.
 // when registering a handler, as all handlers, hooks, filters
 // are already relative to plugins.
@@ -14,13 +17,13 @@ plugins.wcjsonapi_sr = {
   item_list_column: function (params) {
     var item = params.item;
     var col = params.column;
-    Salor.log_action("Params are: " + JSON.stringify(params));
+    Salor.log_action_plugin("wcjsonapi_sr: Params are: " + JSON.stringify(params));
     if ( col['name'] == 'wcjsonapi_sr_send_to_wp') {
-      Salor.log_action("col name was correct");
-      params.column = '<img class="wc-jsonapi-sr-send-item-button" style="cursor:pointer;" id="send_to_wp_for_' + item.id + '" src="' + URLS.images + '/wcjsonapi_sr/wordpress_logo.svg" height="16"/>';
+      Salor.log_action_plugin("wcjsonapi_sr: col name was correct");
+      params.column = '<img class="wc-jsonapi-sr-send-item-button" style="cursor:pointer;" id="send_to_wp_for_' + item.id + '" src="' + PLUGINS_BASE_URL + '/wcjsonapi_sr/wordpress_logo.svg" height="16"/>';
       params.column += '<script type="text/javascript">$("#send_to_wp_for_' + item.id + '").on("click",function () { wcjsonapi_sr.send_to_wp($(this),'+ JSON.stringify(item) +');});</script>'
     } else {
-      Salor.log_action("was not correct");
+      Salor.log_action_plugin("wcjsonapi_sr: was not correct");
     }
     return params;
   },
